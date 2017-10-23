6ca2339b0d0298c8500f610fdcb2bc7fadcf8cc1
Brad Slater <bslater@financialforce.com>
2017-10-23 11:04:07 +0100

    fixed issues with schema

--- template/src/lib/handlers/api/ageAndDob.js
+++ template/src/lib/handlers/api/ageAndDob.js
@@ -3,7 +3,7 @@
 const debug = require('debug-plus')('financialforcedev:orizuru~tools:example:handlers:api:test');
 
 module.exports = ({ message, context }) => {
-	debug.log('Handled event for schema \'api/test\'...');
+	debug.log('Handled event for schema \'api/ageAndDob\'...');
 	debug.log('Context:');
 	debug.log(JSON.stringify(context));
 	debug.log('Message:');
