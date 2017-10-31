e3a5d7e62bc4a226c90663ce5021860786a6edcb
Peter Wright <pwright@financialforce.com>
2017-10-31 16:20:20 +0000

    WIP: Add test file

---
+++ src/spec/service/deploy/properties.spec.js
@@ -0,0 +1,131 @@
+/**
+ * Copyright (c) 2017, FinancialForce.com, inc
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, 
+ *   are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice, 
+ *      this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice, 
+ *      this list of conditions and the following disclaimer in the documentation 
+ *      and/or other materials provided with the distribution.
+ * - Neither the name of the FinancialForce.com, inc nor the names of its contributors 
+ *      may be used to endorse or promote products derived from this software without 
+ *      specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
+ *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
+ *  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL 
+ *  THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+ *  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ *  OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ *  OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ **/
+
+'use strict';
+
+const
+	chai = require('chai'),
+	chaiAsPromised = require('chai-as-promised'),
+	root = require('app-root-path'),
+	sinon = require('sinon'),
+	sinonChai = require('sinon-chai'),
+	proxyquire = require('proxyquire'),
+
+	path = require('path'),
+
+	expect = chai.expect,
+
+	sandbox = sinon.sandbox.create(),
+
+	properties = require(root + '/src/lib/service/deploy/properties');
+
+chai.use(chaiAsPromised);
+chai.use(sinonChai);
+
+describe('service/deploy/properties.js', () => {
+
+	let mocks,
+		filepath = path.resolve(process.cwd(), 'local.run.properties');
+
+	beforeEach(() => {
+
+		mocks = {};
+	});
+
+	afterEach(() => {
+		sandbox.restore();
+	});
+
+	describe('updateProperties', () => {
+
+		it('should filter the properties file', () => {
+			const
+				expectedInput = {
+					certificate: {
+						privateKey: 'privateKey'
+					},
+					connectedApp: {
+						oauthConfig: {
+							consumerKey: 'consumerKey'
+						}
+					}
+				},
+				expectedOutputRead = {
+					certificate: {
+						privateKey: 'privateKey'
+					},
+					connectedApp: {
+						oauthConfig: {
+							consumerKey: 'consumerKey'
+						}
+					},
+					properties: {
+						filepath: filepath,
+						content: [
+							'JWT_SIGNING_KEY=privateKey',
+							'OPENID_CLIENT_ID=consumerKey',
+							'OPENID_ISSUER_URI=https://test.salesforce.com/',
+							'OPENID_HTTP_TIMEOUT=4000'
+						]
+					}
+				},
+				expectedOutputWrite = {
+					certificate: {
+						privateKey: 'privateKey'
+					},
+					connectedApp: {
+						oauthConfig: {
+							consumerKey: 'consumerKey'
+						}
+					},
+					properties: {
+						filepath: filepath,
+						content: [
+							'#JWT_SIGNING_KEY=privateKey',
+							'#OPENID_CLIENT_ID=consumerKey',
+							'#OPENID_ISSUER_URI=https://test.salesforce.com/',
+							'#OPENID_HTTP_TIMEOUT=4000',
+							'JWT_SIGNING_KEY=privateKey',
+							'OPENID_CLIENT_ID=consumerKey',
+							'OPENID_ISSUER_URI=https://test.salesforce.com/',
+							'OPENID_HTTP_TIMEOUT=4000'
+						]
+					}
+				};
+
+			properties.readProperties.resolves(expectedOutputRead);
+			properties.writeProperties.resolves(expectedOutputWrite);
+
+			// when - then
+			return expect(properties.updateProperties(expectedInput))
+				.to.eventually.eql(expectedOutputWrite)
+				.then(() => {
+					expect(properties.readProperties).to.be.calledOnce();
+					expect(properties.writeProperties).to.be.calledOnce();
+				});
+		});
+	});
+});
