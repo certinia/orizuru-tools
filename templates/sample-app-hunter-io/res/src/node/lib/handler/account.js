711081f3cb8c78bc64a08f574d549a7a35bfcff3
David Frudd <dfrudd@financialforce.com>
2017-12-14 14:28:05 +0000

    Switch hunter handler to async functions, with more comments

--- templates/sample-app-hunter-io/res/src/node/lib/handler/account.js
+++ templates/sample-app-hunter-io/res/src/node/lib/handler/account.js
@@ -14,70 +14,26 @@ const
 	CONTACT_SOBJECT_NAME = 'Contact',
 	ACCOUNT_SOBJECT_NAME = 'Account',
 
-	newJsforceConnection = credentials => new jsforce.Connection(credentials),
-	getJsforceConnection = user => grant(user).then(newJsforceConnection),
+	hunter = new Hunter(HUNTER_KEY),
 
-	getAccountDomain = (conn, id) => () => {
-		return conn.sobject(ACCOUNT_SOBJECT_NAME).retrieve(id)
-			.then(account => parse(account.Website).domain)
-			.catch(error => null);
-	},
-
-	getEmails = domain => {
-		if (!domain) {
-			return [];
-		}
+	hunterDomainSearchAsync = domain => {
+		// promisify hunter.domainSearch...
+		// we cannot just use util.promisify because hunterio builds the function
+		// dynamically, without named parameters
 		return new Promise((resolve, reject) => {
-			var hunter = new Hunter(HUNTER_KEY);
-
 			hunter.domainSearch({
 				domain
 			}, (err, body) => {
 				if (err) {
 					reject(err);
 				} else {
-					resolve(body.data.emails);
+					resolve(body);
 				}
 			});
 		});
-	},
-
-	getContactsFromEmails = id => emails => {
-		return emails
-			.filter(email => {
-				return email.type === 'personal' &&
-					email.confidence > CONFIDENCE &&
-					email.last_name &&
-					email.first_name;
-			})
-			.map(email => {
-				return {
-					firstName: email.first_name,
-					lastName: email.last_name,
-					accountId: id,
-					email: email.value,
-					title: email.position
-				};
-			});
-	},
-
-	createContacts = conn => contacts => {
-
-		const
-			processChunck = (chain, tenContacts) => {
-				return chain
-					.then(() =>
-						Promise.all(
-							_.map(tenContacts, contact => conn.sobject(CONTACT_SOBJECT_NAME).create(contact))
-						)
-					);
-			};
-
-		return (_.reduce(_.chunk(contacts, 10), processChunck, Promise.resolve()))
-			.then(() => debug.log('success!')).catch(debug.error);
 	};
 
-module.exports = ({ message, context }) => {
+async function handle({ message, context }) {
 	debug.log('Handled event for schema \'api/account\'...');
 	debug.log('Context:');
 	debug.log(JSON.stringify(context));
@@ -85,14 +41,54 @@ module.exports = ({ message, context }) => {
 	debug.log(JSON.stringify(message));
 	debug.log(`Confidence Threshold: ${CONFIDENCE}`);
 
-	return getJsforceConnection(context.user)
-		.then(conn => {
-			return _.reduce(message.ids, (chain, id) => {
-				return chain
-					.then(getAccountDomain(conn, id))
-					.then(getEmails)
-					.then(getContactsFromEmails(id))
-					.then(createContacts(conn));
-			}, Promise.resolve());
-		});
-};
+	try {
+		const
+			// get credentials for the SF org which sent the request
+			credentials = await grant(context.user),
+			// establish the connection
+			connection = new jsforce.Connection(credentials),
+			// read accounts for the supplied Ids
+			accounts = await connection.sobject(ACCOUNT_SOBJECT_NAME).retrieve(message.ids),
+			// build Promises that will return the contacts for each Account
+			createContactPromises = _.map(accounts, async account => {
+				// parse out the domain of the website
+				const domain = parse(account.Website).domain;
+				if (domain) {
+					debug.log('Searching for emails from %s', domain);
+					// query hunter.io for emails for the domain
+					const body = await hunterDomainSearchAsync(domain);
+
+					return body.data.emails
+						// reject emails without first and last name, or below our confidence threshold
+						.filter(x => x.confidence > CONFIDENCE && x.first_name && x.last_name)
+						// create a JSON object representing the Contact SObject record
+						.map(x => ({
+							firstName: x.first_name,
+							lastName: x.last_name,
+							accountId: account.id,
+							email: x.value,
+							title: x.position
+						}));
+				}
+
+				// return an empty array if the website was missing, or gave an invalid domain.
+				return [];
+			}),
+			// wait for the Contacts to be created, resulting in an array where each element
+			// is an Array of Contacts related to a single Account
+			contactArrays = await Promise.all(createContactPromises),
+			// flatten the array of arrays into a single array
+			contacts = _.flatMap(contactArrays);
+
+		if (contacts.length) {
+			// insert the new Contacts
+			await connection.sobject(CONTACT_SOBJECT_NAME).create(contacts);
+			//log the result
+			debug.log('Successfully inserted %s contacts', contacts.length);
+		}
+	} catch (error) {
+		debug.error(error);
+	}
+}
+
+module.exports = handle;
